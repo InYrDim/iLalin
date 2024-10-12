d60769089515432c790541811461ff93a6a045d4
dimjad <230209501007@student.unm.ac.id>
2024-10-13 03:04:35 +0800

    adjust header and nav UI color in users

--- users/components/header.php
+++ users/components/header.php
@@ -1,9 +1,9 @@
 <header class=" header border-bottom shadow-sm" id="header" style="z-index: 999999;">
     <div class="header_toggle">
-        <i class='bx bx-menu' id="header-toggle"></i>
+        <i class="ri-menu-fold-4-line" id="header-toggle"></i>
     </div>
     <div class="d-flex flex gap-3 align-items-center">
-        <span class="ts-uppper"><?= $profile['nama'] ?></span>
+        <span class="ts-uppper text-primary fw-bold"><?= $profile['nama'] ?></span>
         <div class="header_img">
             <img src="<?= strpos($profile['profile_image'], 'data:image') === 0 ? $profile['profile_image'] : 'data:image/jpeg;base64,' . $profile['profile_image'] ?>"
                 alt="<?= $profile["nama"] ?>">
@@ -22,7 +22,7 @@
             <div class="nav_list">
                 <a href="index.php" class="nav_link <?= $active_page == "dashboard" ? "active" : "" ?>">
                     <i class="ri-dashboard-horizontal-line nav_icon"></i>
-                    <span class="nav_name">Dashboard</span>
+                    <span class="nav_name ">Dashboard</span>
                 </a>
                 <a href="profile.php" class="nav_link <?= $active_page == "users" ? "active" : "" ?>">
                     <i class="ri-user-line nav_icon"></i>
